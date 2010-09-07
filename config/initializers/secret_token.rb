a26ee167cf1ac3fa600a7a7f545ace341a99320e
HooleyHoop <steven.hooley@gmail.com>
2010-09-07 14:56:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpendTracker::Application.config.secret_token = 'efe8788143fa77eb31a185ecee53f9467d918696ebe7b0c306a29be646fe6448d923052741b67be43f329a645f4e5e87502c5356bd0865f0223c8ae6933fe023'
